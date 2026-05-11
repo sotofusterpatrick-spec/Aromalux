2a27a6d73dad591e1fa96e5aea206ddb5eae2b0a
whois31 <pieromeza31@gmail.com>
2026-05-11 06:17:36 -0500

    bancos

--- AromaLux/tienda de perfumes/script.js
+++ AromaLux/tienda de perfumes/script.js
@@ -1,8 +1,13 @@
 window.onload = function() {
-    let nombre = prompt("¡Bienvenido a AromaLux! ¿Cómo te llamas?");
+    const currentPage = window.location.pathname.split("/").pop();
+    if (currentPage === "" || currentPage === "index.html") {
+        let nombre = prompt("¡Bienvenido a AromaLux! ¿Cómo te llamas?");
 
-    if (nombre !== null && nombre !== "") {
-        const cajaTexto = document.getElementById("saludo");
-        cajaTexto.innerText = `¡Descubre tu esencia, ${nombre}!`;
+        if (nombre !== null && nombre !== "") {
+            const cajaTexto = document.getElementById("saludo");
+            if (cajaTexto) {
+                cajaTexto.innerText = `¡Descubre tu esencia, ${nombre}!`;
+            }
+        }
     }
 };
